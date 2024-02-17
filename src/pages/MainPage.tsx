c622cb8ab237d4367be722ffc249a2abb14af548
Mr. V <vaidikbhesaniya1212@gmail.com>
2024-02-18 00:31:29 +0530

    feat/check-01

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import usersStore from "../stores/usersStore";
+import React, { useRef, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import usersStore from "../stores/usersStore";
 // components
 import SideBar from "../components/SideBar";
 import HomeScreen from "../components/HomeScreen";
